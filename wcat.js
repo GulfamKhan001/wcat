beb3e7687e99972af0f869ad06011f6fb8082916
GulfamKhan001 <gulfamkajla@gmail.com>
2022-03-09 12:40:54 +0530

    1st and 2nd commands done

--- wcat.js
+++ wcat.js
@@ -7,6 +7,33 @@
 // index    0      1      2      3
 //         node wcat.js f1.txt f2.txt
 
-
+let fs=require("fs");
 let n=process.argv.slice(2);
-console.log(n);
+// console.log("Files to be read are: "+n);
+
+let filearr=[];
+for (let i = 0; i < n.length; i++) {
+    filearr.push(n[i]);
+}
+
+// check if file exists or not
+
+for (let i=0; i<n.length ;i++){
+    let exists = fs.existsSync(n[i]);
+    if (!exists) {
+        console.log(n[i]+" doesnot exists.");
+        //break;
+    }
+}
+
+
+// Display the content and appending.
+
+let content="";
+
+for (let i = 0; i < n.length; i++) {
+    let file=fs.readFileSync(n[i]);
+    content+=file+"\n";
+}
+
+console.log(content);
